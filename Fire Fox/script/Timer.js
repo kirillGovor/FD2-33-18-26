e29b8f452b39cddea7a749b9df40a82cee469fec
kirillGovor <bns223@mail.ru>
2018-08-15 20:16:39 +0300

    commit

--- Fire Fox/script/Timer.js
+++ Fire Fox/script/Timer.js
@@ -1,7 +1,66 @@
+if ( !window.requestAnimationFrame ) {
+
+    window.requestAnimationFrame = ( function() {
+  
+      return window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
+  
+        window.setTimeout( callback, 1000 / 60 );
+  
+      };
+  
+    })();
+  }
+  var hash={
+    cloudSpeed : 2, //скорость облочков
+    cloudstep :0, //счетчик облочка
+    CloudWay : 400, // длинна пути облочка
+    speedY : 5,
+    width : 50,
+    height: 50,
+    OnePlayer:0,
+    TwoPlayer:0,
+    OneSpeed:0,
+    TwoSpeed:0,
+    stopBall:0,
+    OnePlayerGol:0,
+    TwoPlayerGol:0,
+}
+
 function cloudAnimation(){
-    var cloud = document.getElementById;
-    requestAnimationFrame(cloudAnimation) 
+    var cloud = document.getElementById("cloud");
+    var cloud_left; //счетчик, который будет сдивагить облочки
+
+      cloud_left=Number(cloud.style.marginLeft.replace(/[^-0-9]/gim,''));// положение блоков в y , тк они с окончанием px, берем только числа
+      
+      if (cloud_left==0||cloud_left<=hash.CloudWay && hash.cloudstep ==0){ // если облочко слишком далеко,, то его не сдивгаем
+        cloud.style.marginLeft=cloud_left+hash.cloudSpeed+"px";// сдиваем облочко на 2 px
+      }
+      else{
+        cloud.style.marginLeft=cloud_left-hash.cloudSpeed+"px";
+        hash.cloudstep =1
+        if (cloud_left<=2){
+          hash.cloudstep =0;
+        }
+        
+      }
+    
+        
+       
+      
+    requestAnimationFrame(cloudAnimation);
+
+}
+
+function tick(){ //таймер
+
 }
 
 
-requestAnimationFrame(cloudAnimation) 
+
+
+
+
--- Fire Fox/script/menu.js
+++ Fire Fox/script/menu.js
@@ -1,20 +1,22 @@
-function elementMenu(){
+function elementMenu(){ //добовляем все компоненты на страницу
    var body=document.getElementsByTagName("body")
    document.body.innerHTML="<div id=ImportantMenuDiv class=fon><h1 class=menu><span>fire</span> fox</h1><div class=menu_div ><ul id=mainMenu class=OptionsGame><li><a class=link onclick=startGame()  >start game</a></li><li><a class=link onclick=optionsGame() >options</a></li><li><a class=link onclick=closeGame() >exit game</a></li></ul><ul><li><a class=link onclick=music() >music</a></li> </ul></div></div>";
 }
 
 
-function startGame(){
+function startGame(){ //скрыть менюху при нажатии кнопки старт
     var menu_block =document.getElementById("ImportantMenuDiv");
     
         menu_block.style.visibility="hidden";
     
 }
 
-function optionsGame(){
+function optionsGame(){ //скрывает тескт при нажатии настроек
     var menu_block =document.getElementById("mainMenu");
-  if (menu_block.style.visibility=="hidden"){
-    menu_block.style.visibility="visible";
+    var option_menu= document.getElementById("optionMenu")
+  if (menu_block.style.display=="block"||menu_block.style.display==""){
+    menu_block.style.display="none";
+    option_menu.style.display="block"
   }
   else{
     
@@ -30,5 +32,28 @@ function closeGame(){
 
 function optionmenuMusic(){
     var music=document.getElementById("music_link");
+    if(music.innerHTML=="music: on"){
+    music.innerHTML="music: off"
+    }
+    else{
+    music.innerHTML="music: on"
+    }
     
+}
+
+function optionmenuSounds(){
+    var sounds=document.getElementById("sound_link");
+    if(sounds.innerHTML=="sounds: on"){
+        sounds.innerHTML="sounds: off"
+    }
+    else{
+        sounds.innerHTML="sounds: on"
+    }
+    
+}
+function optionmenuBack(){
+    var menu_block =document.getElementById("mainMenu");
+    var option_menu= document.getElementById("optionMenu");
+    menu_block.style.display="block";
+    option_menu.style.display="none";
 }
